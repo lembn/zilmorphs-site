6dac10f21abe26daf4456bbbe77de96c4efa9536
MszBednarski <msz.bednarski@gmail.com>
2021-09-28 14:56:15 +0200

    usdt address

--- data/addressbook.ts
+++ data/addressbook.ts
@@ -2,12 +2,12 @@ import { ByStr20 } from "boost-zil";
 
 export const addressbook = {
     ethTOKEN: new ByStr20("zil19j33tapjje2xzng7svslnsjjjgge930jx0w09v"),
-    usdTOKEN: new ByStr20("zil12fntnfaawaev0l6edcd8uk6vfxrjau3j8pj7fw"),
+    usdTOKEN: new ByStr20("zil1sxx29cshups269ahh5qjffyr58mxjv9ft78jqy"),
     btcTOKEN: new ByStr20("zil1wha8mzaxhm22dpm5cav2tepuldnr8kwkvmqtjq"),
     ZILMORPHS_ADDRESS: new ByStr20(
         "zil1s5kyzptxp2eg35xl3vjfra6x93n2rs9wd3gwal"
     ),
     ETH_SELLER: new ByStr20("zil1dn3mhzmx5d2xqru00yxxs85g5yz0dphttpjwtv"),
     BTC_SELLER: new ByStr20("zil16l2zlkltca3q6j972qlncpdl87sgz6n0gx3kwv"),
-    USDT_SELLER: new ByStr20("zil1n390avjx4qtfacsnyrhk78dzjfmzsm85ak660v"),
+    USDT_SELLER: new ByStr20("zil18ln7n2me3zxrc5y5zdgvj4ryawat0g2p3qzcr6"),
 };
