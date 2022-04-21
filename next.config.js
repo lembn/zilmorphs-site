ac88ec425c5c3121d124922494d82bfea8fa813a
lembn <lembn@outlook.com>
2022-04-21 13:45:54 +0100

    update config for node 16

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 module.exports = {
     webpack: (config, { isServer }) => {
         // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
-            config.node = {
-                fs: 'empty'
-            }
-        }
-        return config
-    }
-}
+        if (!isServer) config.resolve.fallback.fs = false;
+        return config;
+    },
+};
