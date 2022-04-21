9511e2af0cc4f8de43c939fdb3c338255f34f040
Miron Bykov <miron.bykov@outlook.com>
2022-04-21 13:57:11 +0100

    initialization - miron

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     webpack: (config, { isServer }) => {
         // Fixes npm packages that depend on `fs` module
-        if (!isServer) {
+        if (!isServer) { 
             config.node = {
                 fs: 'empty'
             }
