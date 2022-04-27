f65525ca68d47c09d3d14a4afa0b1c8166cc9526
lembn <lembn@outlook.com>
2022-04-27 11:42:44 +0100

    review pr

--- pages/dapp/arcade.tsx
+++ pages/dapp/arcade.tsx
@@ -3,89 +3,67 @@ import { Box, Grid, Heading, ResponsiveContext } from "grommet";
 import { GiCrossedSwords, GiCardRandom } from "react-icons/gi";
 import Typewriter from "typewriter-effect";
 import router from "next/router";
-import React from "react";
+import React, { useContext } from "react";
 
 export default observer(() => {
-  // add responsiveness to the boxes that contain arcade options
-  function highlightBox(e) {
-    e.target.style.borderWidth = "5px";
-  }
+  const bpSize = useContext(ResponsiveContext);
 
-  function dehighlightBox(e) {
-    e.target.style.borderWidth = "3px";
+  function Game({ name, inner }: { name: string; inner: React.ReactNode }) {
+    return (
+      <Box
+        margin={bpSize == "small" ? { left: "100px", right: "-100px" } : "none"}
+        gridArea={name.toLowerCase()}
+        focusIndicator={false}
+        border={{ color: "black", size: "3px", side: "all" }}
+        onMouseOver={(e) => ((e.target as HTMLElement).style.borderWidth = "5px")}
+        onMouseLeave={(e) => ((e.target as HTMLElement).style.borderWidth = "3px")}
+        onClick={() => router.push(`/dapp/${name.toLowerCase()}`)}
+      >
+        <Heading alignSelf="center" margin="medium" size="xsmall">
+          {name}
+        </Heading>
+        <Heading alignSelf="center" margin="none" size="xsmall">
+          {inner}
+        </Heading>
+      </Box>
+    );
   }
-  // track the size of the window in a variable for resizing components
-  const context = React.useContext(ResponsiveContext);
 
   return (
-    <Grid
-      alignSelf="center"
-      // each item in the array is the size for one specific row/column
-      rows={context === "small" ? ["xsmall", "small", "small"] : ["xsmall", "small"]}
-      columns={context === "small" ? ["small", "small"] : ["medium", "medium"]}
-      gap="medium"
-      // coordinates are stored as [column coord, row coord], the column coordinates change depending on screen size
-      areas={
-        context === "small"
-          ? [
-              { name: "typewriter", start: [0, 0], end: [1, 0] },
-              { name: "morphsino", start: [0, 1], end: [0, 1] },
-              { name: "battle", start: [0, 2], end: [0, 2] },
-            ]
-          : [
-              { name: "typewriter", start: [0, 0], end: [1, 0] },
-              { name: "morphsino", start: [0, 1], end: [0, 1] },
-              { name: "battle", start: [1, 1], end: [1, 1] },
-            ]
-      }
-    >
-      <Box gridArea="typewriter">
-        {/* //type writer effect for the title of the page */}
-        <Heading alignSelf="center" margin={"none"}>
-          <Typewriter
-            options={{
-              // strings that are displayed by typewriter
-              strings: ["Arcade", "Unleash the beast", "Test your luck", "There is only one Champion", "Exceed your limits", "Evolve"],
-              autoStart: true,
-              loop: true,
-            }}
-            onInit={(typewriter) => typewriter.start()}
-          />
-        </Heading>
-      </Box>
-      {/* // boxes presenting the options of the arcade */}
-      <Box
-        margin={context == "small" ? { left: "100px", right: "-100px" } : "none"}
-        gridArea="morphsino"
-        focusIndicator={false}
-        border={{ color: "black", size: "3px", side: "all" }}
-        onMouseOver={highlightBox}
-        onMouseLeave={dehighlightBox}
-        onClick={() => router.push("/dapp/play")}
+    <>
+      <Heading alignSelf="center">
+        <Typewriter
+          options={{
+            strings: ["Arcade", "Unleash the beast", "Test your luck", "There is only one Champion", "Exceed your limits", "Evolve"],
+            autoStart: true,
+            loop: true,
+          }}
+          onInit={(typewriter) => typewriter.start()}
+        />
+      </Heading>
+
+      <Grid
+        alignSelf="center"
+        // each item in the array is the size for one specific row/column
+        rows={bpSize == "small" ? ["xsmall", "small", "small"] : ["xsmall", "small"]}
+        columns={bpSize == "small" ? ["small", "small"] : ["medium", "medium"]}
+        gap="medium"
+        // coordinates are stored as [column coord, row coord], the column coordinates change depending on screen size
+        areas={
+          bpSize == "small"
+            ? [
+                { name: "morphsino", start: [0, 1], end: [0, 1] },
+                { name: "battle", start: [0, 2], end: [0, 2] },
+              ]
+            : [
+                { name: "morphsino", start: [0, 1], end: [0, 1] },
+                { name: "battle", start: [1, 1], end: [1, 1] },
+              ]
+        }
       >
-        <Heading alignSelf="center" margin={"medium"} size={"xsmall"}>
-          Morphsino
-        </Heading>
-        <Heading alignSelf="center" margin={"none"} size={"xsmall"}>
-          <GiCardRandom />
-        </Heading>
-      </Box>
-      <Box
-        margin={context == "small" ? { left: "100px", right: "-100px" } : "none"}
-        gridArea="battle"
-        focusIndicator={false}
-        border={{ color: "black", size: "3px", side: "all" }}
-        onMouseOver={highlightBox}
-        onMouseLeave={dehighlightBox}
-        onClick={() => router.push("/dapp/battle")}
-      >
-        <Heading alignSelf="center" margin={"medium"} size={"xsmall"}>
-          Battle
-        </Heading>
-        <Heading alignSelf="center" margin={"none"} size={"xsmall"}>
-          <GiCrossedSwords />
-        </Heading>
-      </Box>
-    </Grid>
+        <Game name="Morphsino" inner={<GiCardRandom />} />
+        <Game name="Battle" inner={<GiCrossedSwords />} />
+      </Grid>
+    </>
   );
 });
